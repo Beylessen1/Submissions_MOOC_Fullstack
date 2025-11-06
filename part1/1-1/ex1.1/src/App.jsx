8bca79a071415680d2836884f404b18a624811b6
Beylessen1 <beylessenjendoubi01@gmail.com>
2025-11-06 16:03:48 +0100

    part1 ex1

---
+++ part1/1-1/ex1.1/src/App.jsx
@@ -0,0 +1,40 @@
+function Header(props){
+  return <h1>{props.course}</h1>;
+}
+
+function Content(props){
+  return <p>{props.name} {props.number}</p>
+}
+
+
+function Total(props){
+  return <p>Number of exercises {props.total}</p>
+  
+}
+
+
+
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {name: 'Fundamentals of React', number: 10},
+    {name: 'Using props to pass data' , number: 7},
+    {name: 'State of a component' , number: 14},
+  ]
+
+  return (
+    <div>
+      <Header course={course}/>
+
+      <Content name={parts[0].name} number={parts[0].number}/>
+      <Content name={parts[1].name} number={parts[1].number}/>
+      <Content name={parts[2].name} number={parts[2].number}/>
+
+      <Total total={parts[0].number + parts[0].number + parts[0].number}/>
+    </div>
+  )
+}
+
+export default App
